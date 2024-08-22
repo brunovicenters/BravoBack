07ded88278ce0afdebd57635c91d11da3d310023
bruno <brunovicente.dev@gmail.com>
2024-08-22 20:09:58 -0300

    Added Resources

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProdutoIndexResource;
+use App\Http\Resources\ProdutoShowResource;
 use App\Models\Produto;
 use Illuminate\Http\Request;
 
@@ -9,11 +11,17 @@ class ProdutoController extends Controller
 {
     public function index()
     {
-        //
+        $promo = true;
+        $price = 25;
+        $categoria = 3;
+
+        $request = [$promo, $price, $categoria];
+
+        return new ProdutoIndexResource($request);
     }
 
     public function show(Produto $produto)
     {
-        //
+        return new ProdutoShowResource($produto);
     }
 }
