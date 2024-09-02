5bdb1145a6cf79ae63c7668d3f922471e60d25ea
bruno <brunovicente.dev@gmail.com>
2024-09-02 15:50:42 -0300

    Added MaisVendidas method

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -15,4 +15,37 @@ class Categoria extends Model
     public $timestamps = false;
 
     public $guarded = [];
+
+    public function Produtos()
+    {
+        return $this->hasMany(Produto::class, 'CATEGORIA_ID', 'CATEGORIA_ID');
+    }
+
+    public static function MaisVendidas($limit)
+    {
+        $maisVendidas = Produto::ProdutoValido()
+            ->selectRaw("PRODUTO.CATEGORIA_ID, SUM(PRODUTO.PRODUTO_VENDAS) as sales_qty")
+            ->groupBy("PRODUTO.CATEGORIA_ID")
+            ->orderBy("sales_qty", "desc")
+            ->limit($limit)
+            ->get();
+
+        foreach ($maisVendidas as $value) {
+            $produtosCategorias[$value->CATEGORIA_ID] = Produto::ProdutoValido()
+                ->where('PRODUTO.CATEGORIA_ID', $value->CATEGORIA_ID)
+                ->orderBy('PRODUTO_VENDAS', 'desc')
+                ->take(5)
+                ->get()
+                ->map(function ($produto) {
+                    return [
+                        'PRODUTO_NOME' => $produto->PRODUTO_NOME,
+                        'PRODUTO_PRECO' => $produto->PRODUTO_PRECO,
+                        'PRODUTO_DESCONTO' => $produto->PRODUTO_DESCONTO,
+                        'IMAGEM_URL' => $produto->Imagem[0]->IMAGEM_URL
+                    ];
+                });;
+        }
+
+        return $produtosCategorias;
+    }
 }
