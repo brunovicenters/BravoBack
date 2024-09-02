8131f351cb1520e91d2a4183410e9e2ced2ba3d9
bruno <brunovicente.dev@gmail.com>
2024-09-02 15:50:57 -0300

    Added Home index route and resource

---
+++ app/Http/Resources/HomeResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Categoria;
+use App\Models\Produto;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HomeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $promocao = Produto::ProdutoValido()
+            ->where('PRODUTO_DESCONTO', '>', 0)
+            ->orderBy('PRODUTO_DESCONTO', 'desc')
+            ->take(5)
+            ->get();
+
+        $produtosMaisVendidos = Produto::MaisVendido(5);
+
+        $categoriasMaisVendidas = Categoria::MaisVendidas(3);
+
+        return [
+            'promocao' => $promocao,
+            'produtosMaisVendidos' => $produtosMaisVendidos,
+            'categoriasMaisVendidas' => $categoriasMaisVendidas
+        ];
+    }
+}
