21fa6609c5aff517162a09f446ac4121c916951a
bruno <brunovicente.dev@gmail.com>
2024-10-11 18:34:38 -0300

    Added Carrinho index

--- app/Http/Resources/CarrinhoIndexResource.php
+++ app/Http/Resources/CarrinhoIndexResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Carrinho;
+use App\Models\Produto;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +16,26 @@ class CarrinhoIndexResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [];
+
+        $id = $this->resource;
+
+        $produtos = Produto::with("Imagem")
+            ->ProdutoValido()
+            ->join('CARRINHO_ITEM', 'PRODUTO.PRODUTO_ID', '=', 'CARRINHO_ITEM.PRODUTO_ID')
+            ->where('CARRINHO_ITEM.USUARIO_ID', $id)
+            ->get()
+            ->map(function ($item) {
+                return [
+                    'id' => $item->PRODUTO_ID,
+                    'name' => $item->PRODUTO_NOME,
+                    'quantity' => $item->ITEM_QTD,
+                    'price' => $item->PRODUTO_PRECO,
+                    'image' => $item->IMAGEM->first()->IMAGEM_URL
+                ];
+            });
+
+        return [
+            'carrinho' => $produtos
+        ];
     }
 }
